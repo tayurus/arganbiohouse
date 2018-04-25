73e93d1ee5e8633ebb5143a4870fcef047db0fd5
tayurus <tayurus@gmail.com>
2018-04-25 10:34:19 +0300

    header is ready

---
+++ blocks/header/header.js
@@ -0,0 +1,24 @@
+$(document).ready(function functionName() {
+    $(".search-icon").click(function(event) {
+        event.stopPropagation();
+        $(".search-icon form").toggle();
+        $(".modal-wrapper").show();
+    });
+
+    $(".search-icon .search-clear").click(function() {
+        event.stopPropagation();
+        $(".search-icon input").val("");
+        $(".search-icon form").toggle();
+        // $(".modal-wrapper").hide();
+    });
+
+    $(".search-icon input").click(function(event) {
+        event.stopPropagation();
+    })
+
+    $(".modal-wrapper").click(function() {
+        $(".search-icon form").hide();
+        $(".modal-wrapper").hide();
+    })
+
+});
