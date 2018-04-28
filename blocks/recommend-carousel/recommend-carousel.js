9b8f235e679411247c95803c2465cc96847f7699
tayurus <tayurus@gmail.com>
2018-04-28 22:20:56 +0300

    add prototype of recommend-carousel

---
+++ blocks/recommend-carousel/recommend-carousel.js
@@ -0,0 +1,14 @@
+$(".recommend-carousel").owlCarousel({
+    items: 1,
+    nav: true,
+    dots: false,
+    responsive:{
+        600:{
+            items: 2
+        },
+        900:{
+            items:3
+        }
+    }
+})
+<>
